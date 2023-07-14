af20d0f795ef4bd348abd70eb83978f6809f7745
Amr Adel <amr96.adel@gmail.com>
2023-07-14 10:17:36 -0700

    5th task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @Phrase - The required phrase to be printed
+ * Return: Always 0 (Success)
+ */
+
+int main()
+	{
+	char Phrase[]= "\"Programming is like building a multilingual puzzle";
+	puts(Phrase);
+	return 0;
+	}
