97d8e39f10e40925bd1e6c6aec00ec5e75f5fe04
Amr Adel <amr96.adel@gmail.com>
2023-07-14 14:58:39 -0700

    Task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * @x -intiger numbr
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	
+	printf("\n");
+	return (0);
+}
