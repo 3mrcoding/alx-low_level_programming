05b23dbd7ef827bc65d2a2d8d2b587d9587af078
Amr Adel <amr96.adel@gmail.com>
2023-07-14 14:23:38 -0700

    Task 03

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+	char ii;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+
+	for (ii = 'A'; x <= 'Z'; x++)
+	{
+		putchar(ii);
+	}
+	putchar('\n');
+	return (0);
+}
