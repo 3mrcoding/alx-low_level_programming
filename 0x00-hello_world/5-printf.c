0eed1f96ecc83faec4a6d6e7fd3501cb7247ca09
Amr Adel <amr96.adel@gmail.com>
2023-07-14 10:36:53 -0700

    6th task-2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/*
- *main - Entry point
+/**
+ * main - A program that print a certain phrase
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
