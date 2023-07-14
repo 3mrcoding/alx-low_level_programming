57cb7d57bcfb9e550c3d18bcd9c5fa31770378e2
Amr Adel <amr96.adel@gmail.com>
2023-07-14 15:38:57 -0700

    Task 09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * @x -intiger numbr
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 9)
+		{
+			putchar('0' + x);
+		}
+		else
+		{
+		putchar('0' + x);
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
