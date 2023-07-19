923be8205532845dfa7e6eda6a4754895c3f75da
Amr Adel <amr96.adel@gmail.com>
2023-07-19 14:53:55 -0700

    Task00

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(arr[i]);
+	}
+	return (0);
+}
