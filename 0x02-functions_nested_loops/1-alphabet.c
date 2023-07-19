e890b72cd1ec32fd421eb5eb1b72310345a23e2b
Amr Adel <amr96.adel@gmail.com>
2023-07-19 15:20:47 -0700

    Task01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(0 + i);
+	}
+	_putchar('\n');
+	return (0);
+}
