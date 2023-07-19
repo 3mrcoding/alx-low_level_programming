8c53fa500c73b742a769a25f47f81d877560225d
Amr Adel <amr96.adel@gmail.com>
2023-07-19 16:11:50 -0700

    Task03

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - utilize _putchar function to print alphabet
+ *
+ * Return: Always (0)
+ */
+
+print_alphabet_x10()
+{
+	int i;
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			{
+				_putchar(0 + i);
+			}
+	_putchar('\n');
+	}
+}
