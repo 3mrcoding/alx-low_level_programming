7f9e851f17fedfdcfcb324e609e052cced9994fa
Amr Adel <amr96.adel@gmail.com>
2023-07-20 08:09:17 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adding two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of 2 numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
