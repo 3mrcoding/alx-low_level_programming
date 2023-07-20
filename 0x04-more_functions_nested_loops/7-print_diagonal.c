5ced73663caf4780c5e9245b818ba0581c5e16e2
Amr Adel <amr96.adel@gmail.com>
2023-07-20 15:19:29 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int x;
+		int y;
+
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= x; y++)
+			{
+				_putchar(' ');
+				_putchar('\\');
+				_putchar('\n');
+			}
+			_putchar('\n');
+		}
+	}
+	{
+		_putchar('\n');
+	}
+}
