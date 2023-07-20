34834a4f3f66b88b21c2e774d29d27c1499fda92
Amr Adel <amr96.adel@gmail.com>
2023-07-20 14:38:29 -0700

    Task 09

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,15 +15,16 @@ void print_square(int size)
 	if (size > 0)
 	{
 		for (x = 0; x < size; x++)
-			{
-				for (y = 0; y < size; y++)
-					{
-						_putchar('#');
-					}
-				_putchar('\n');
-			}
 		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
+	else
+	{
+		_putchar('\n');
+	}
 }
-
