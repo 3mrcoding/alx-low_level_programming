e47f38c48d7d4c810e015c860cc44b26712b9cec
Amr Adel <amr96.adel@gmail.com>
2023-07-20 14:29:12 -0700

    task 09

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - print square with certain size
+ *
+ * @size: size of square
+ *
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+			{
+				for (y = 0; y < size; y++)
+					{
+						_putchar('#');
+					}
+				_putchar('\n');
+			}
+		{
+		}
+	}
+}
+
