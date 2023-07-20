c8dc0e1065831e9f402faf244b587342fef76752
Amr Adel <amr96.adel@gmail.com>
2023-07-20 04:55:25 -0700

    Task 06

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - returns absulute value for an integer
+ *
+ * @c: integer
+ *
+ * Return: |n|
+ */
+
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		return (c);
+	}
+	return (-c);
+}
