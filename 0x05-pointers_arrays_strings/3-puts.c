6c4ce2b1883c07b30640c42200f34fc01865f6fe
Amr Adel <amr96.adel@gmail.com>
2023-07-24 23:13:25 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function prints string
+ *
+ * @str: string location in memory
+ *
+ */
+
+void _puts(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
