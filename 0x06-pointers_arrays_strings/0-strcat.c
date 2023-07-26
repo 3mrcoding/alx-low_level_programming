563fb792151d4375ab0ae4b2ccf0c543ee9fedfa
Amr Adel <amr96.adel@gmail.com>
2023-07-26 13:56:38 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len1;
+	int len2;
+	int x;
+
+	for (len1 = 0; dest[len1] != '\0'; len1++)
+	{
+	}
+	for (len2 = 0; src[len2] != '\0'; len2++)
+	{
+	}
+
+	for (x = 0; x < len2; x++)
+	{
+		dest[len1] = src[x];
+		len1++;
+	}
+
+	dest[len1] = '\0';
+
+	return (dest);
+}
