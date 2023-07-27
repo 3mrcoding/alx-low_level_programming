69ac93400ed3cdd77cf2d6663741c7c2f93b8f2a
Amr Adel <amr96.adel@gmail.com>
2023-07-27 14:49:44 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+
+/**
+ * string_toupper - function that changes all lowercase letters of a string to uppercase.
+ *
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+	int x;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+	}
+	for (x = 0; x < i; x++)
+	{
+		int y;
+		y = n[x];
+
+		if (y <= 122 && y >= 97)
+		{
+			y = y - 32;
+			n[x] = y;
+		}
+	}
+	return(n);
+}
