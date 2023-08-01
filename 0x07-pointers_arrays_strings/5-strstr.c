46ff85a2214462501913c4e168f4af8d79ece597
Amr Adel <amr96.adel@gmail.com>
2023-08-01 14:06:02 -0700

    Task 06

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,34 @@
-
+#include "main.h"
 /**
- * _strstr - function to search for a char
- *
- * @haystack: string
- * @needle: word to be searched for
- *
- * Return: null
- *
- */
+*_strstr - The _strstr() function finds the first occurrence
+* of the substring needle in the string haystack.
+* The terminating null bytes (\0) are not compared
+*@haystack: string where the search is made
+*@needle: string whose occurence is searched in haystack
+*Return:Returns a pointer to the beginning of the located
+* substring, or NULL if the substring is not found.
+*/
 
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
-	int x;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
 	{
-		for (x = 0; haystack[x] != '\0'; x++)
+		i = 0;
+
+		if (haystack[i] == needle[i])
 		{
-			if (needle[i] == haystack[x])
-			{
-				return (&needle[i]);
-				x++;
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
 				i++;
-			}
+			} while (haystack[i] == needle[i]);
 		}
+		haystack++;
 	}
 	return ('\0');
 }
-
