7a01cbaccf42dc54cbd6b1e36aa42716b3f9b83a
root <root@DESKTOP-GF1HC1U>
2023-08-05 01:08:12 +0300

    TASK -05

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adding function
+ *
+ * @argc: no. of arguments
+ * @argv: string
+ *
+ * Return: Always (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int i;
+	int y = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		x = atoi(argv[i]);
+
+		if (x == '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			y += x;
+		}
+	}
+	printf("%d\n", y);
+	return (0);
+}
