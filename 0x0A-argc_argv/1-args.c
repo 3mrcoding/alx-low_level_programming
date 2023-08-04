98b73d97dae7d6e9ae64313ab630440fb26d4602
root <root@DESKTOP-GF1HC1U>
2023-08-04 23:08:22 +0300

    TASK -01

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,15 +2,15 @@
 
 /**
  * main - prints the number of arguments
- * 
+ *
  * @argc: number of arguments entered
  * @argv: string entered
- * 
+ *
  * Return: Always (0)
 */
 
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-    printf("%d\n", argc - 1);
-    return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
