d8870d3d4472cf44691e5b899763702632cda274
Amr Adel <amr96.adel@gmail.com>
2023-08-29 23:33:17 +0300

    TASK07

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - print nth elemetn of a linked list
+ * @head: pointer to header of 1st list
+ * @index: index of required list
+ *
+ * Return: pointer to the nth list
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *ptr = head;
+
+	if (ptr == NULL)
+		return (NULL);
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		i++;
+		if (i == index)
+			break;
+	}
+	return (ptr);
+}
