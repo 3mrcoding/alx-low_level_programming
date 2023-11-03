2fa36d19a2f17477c3632bcfa58852ffdb75d943
AMR ADEL <amr96.adel@gmail.com>
2023-11-03 10:45:25 +0200

    TASK 00-02

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,13 +7,26 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	int i = 0;
+	unsigned long int i;
 
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
+	if (ht->array == NULL)
+	{
+		free(ht->array);
+		return (NULL);
+	}
 
-	for (i; i < size; i++)
+	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
+
+	return (ht);
 }
