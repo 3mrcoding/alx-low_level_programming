df24b4553afbc0ed5c41f89272558eaf0a0c2456
AMR ADEL <amr96.adel@gmail.com>
2023-11-03 10:27:29 +0200

    TASK 00-01

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,19 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_t - function creates a hash table
+ * @size: size of hash table
+ * Return: pointer of hash table.
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    int i = 0;
+	int i = 0;
 
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
-    ht ->size = size;
-    ht ->array = malloc(sizeof(hash_node_t) * size);
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-    for (i; i < size; i++)
-        ht ->array[i] = NULL;
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t) * size);
+
+	for (i; i < size; i++)
+		ht->array[i] = NULL;
 }
