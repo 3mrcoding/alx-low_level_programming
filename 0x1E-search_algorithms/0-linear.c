362aa56ea887bd13a6fa86072c2be5f48f5fa67a
Amr Adel <amr96.adel@gmail.com>
2024-02-07 00:10:29 +0200

    TASK 00 - 02

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,14 +18,14 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d] \n", i, array[i]);
-		if (i == size)
-		{
-			return (-1);
-		}
 		if (array[i] == value)
 		{
 			break;
 		}
 	}
+	if (i == size)
+		{
+			return (-1);
+		}
 	return (i);
 }
