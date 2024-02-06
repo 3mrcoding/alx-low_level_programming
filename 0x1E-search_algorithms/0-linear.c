9877dee23368ff8268b6a86f5f6c95cd71b3deaf
Amr Adel <amr96.adel@gmail.com>
2024-02-07 00:03:09 +0200

    TASK 00

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search function
+ * @array: array which will be searched in
+ * @size: size of the array
+ * @value: element to find
+ * Return: index of element  if found, -1 otherwise
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i <= size; i++)
+	{
+		printf("Value checked array[%ld] = [%d] \n", i, array[i]);
+		if (i == size - 1 ||  array == NULL)
+		{
+			return (-1);
+		}
+		if (array[i] == value)
+		{
+			break;
+		}
+	}
+	return (i);
+}
